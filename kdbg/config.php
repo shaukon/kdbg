bb477de8ed8060624cf24b352e1f11d8456ae0ff
shaukon <32252579+shaukon@users.noreply.github.com>
2019-09-18 17:10:33 +0800

    Add files via upload

---
+++ kdbg/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 52818
+ * Date: 2019/9/18
+ * Time: 16:04
+ */
+//数据库主机地址
+define('DB_HOST', 'localhost');
+//数据库用户名
+define('DB_USER', 'root');
+//数据库密码
+define('DB_PASS', 'root');
+//数据库名称
+define('DB_NAME', 'fangtang');
---
+++ kdbg/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 52818
+ * Date: 2019/9/18
+ * Time: 15:58
+ */
+require_once('config.php');
+echo DB_USER;
